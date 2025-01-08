654db40c8c77bd8d4808de48505379b6065a8c2b
Valdiq <stasyshyn.vld@gmail.com>
2025-01-08 02:40:10 +0100

    default schema fixed

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,28 +1,25 @@
--- Table for AddressEntity
-CREATE TABLE address (
+CREATE TABLE IF NOT EXISTS address (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    country VARCHAR(255) NOT NULL,
-    city VARCHAR(255) NOT NULL,
+    country VARCHAR(255),
+    city VARCHAR(255),
     street VARCHAR(255),
     apartment VARCHAR(255),
     postal_code VARCHAR(255)
 );
 
--- Table for DepartmentEntity
-CREATE TABLE department (
+CREATE TABLE IF NOT EXISTS department (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    num_of_emp INT NOT NULL
+    name VARCHAR(255),
+    num_of_emp INT
 );
 
--- Table for EmployeeEntity
-CREATE TABLE employee (
+CREATE TABLE IF NOT EXISTS employee (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    surname VARCHAR(255) NOT NULL,
-    age INT NOT NULL,
+    name VARCHAR(255),
+    surname VARCHAR(255),
+    age INT,
     address_id BIGINT,
     department_id BIGINT,
-    FOREIGN KEY (address_id) REFERENCES address(id) ON DELETE CASCADE,
-    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+    FOREIGN KEY (address_id) REFERENCES address (id),
+    FOREIGN KEY (department_id) REFERENCES department (id)
 );
